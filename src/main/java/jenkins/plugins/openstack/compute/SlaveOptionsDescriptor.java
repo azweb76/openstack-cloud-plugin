9f237b676846c333f3f158fc9f3bba36395269f5
dclayton <dclayton@godaddy.com>
2016-07-28 16:03:32 -0700

    support multiple AZs; remove required field; add Metadata field;

--- src/main/java/jenkins/plugins/openstack/compute/SlaveOptionsDescriptor.java
+++ src/main/java/jenkins/plugins/openstack/compute/SlaveOptionsDescriptor.java
@@ -182,11 +182,6 @@ public final class SlaveOptionsDescriptor extends hudson.model.Descriptor<SlaveO
             @QueryParameter String value,
             @RelativePath("../../slaveOptions") @QueryParameter("floatingIpPool") String def
     ) {
-        if (Util.fixEmpty(value) == null) {
-            String d = getDefault(def, opts().getFloatingIpPool());
-            if (d != null) return FormValidation.ok(def(d));
-            return REQUIRED;
-        }
         return OK;
     }
 
@@ -515,6 +510,6 @@ public final class SlaveOptionsDescriptor extends hudson.model.Descriptor<SlaveO
      */
     @Restricted(DoNotUse.class) // For view
     public @Nonnull String def(@CheckForNull Object val) {
-        return val == null ? "" : ("Inherited vlaue: " + val);
+        return val == null ? "" : ("Inherited value: " + val);
     }
 }
